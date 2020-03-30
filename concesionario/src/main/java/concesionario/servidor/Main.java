b47e76a554983e201e05afe50c6c387b3d5bd043
Javi <javi.martin22@opendeusto.es>
2020-03-30 18:14:04 +0200

    Configurado servidor y añadidos metodos de prueba servidor GET y POST

--- concesionario/src/main/java/concesionario/servidor/Main.java
+++ concesionario/src/main/java/concesionario/servidor/Main.java
@@ -13,7 +13,7 @@ import java.net.URI;
  */
 public class Main {
     // Base URI the Grizzly HTTP server will listen on
-    public static final String BASE_URI = "http://localhost:8080/myapp/";
+    public static final String BASE_URI = "http://localhost:8080/concesionario/";
 
     /**
      * Starts Grizzly HTTP server exposing JAX-RS resources defined in this application.
@@ -22,7 +22,7 @@ public class Main {
     public static HttpServer startServer() {
         // create a resource config that scans for JAX-RS resources and providers
         // in com.example package
-        final ResourceConfig rc = new ResourceConfig().packages("com.example");
+        final ResourceConfig rc = new ResourceConfig().packages("concesionario.servidor");
 
         // create and start a new instance of grizzly http server
         // exposing the Jersey application at BASE_URI
---
+++ concesionario/src/main/java/concesionario/servidor/UsuariosResource.java
@@ -0,0 +1,46 @@
+package concesionario.servidor;
+
+import concesionario.servidor.datos.*;
+import javax.ws.rs.Path;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Produces;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.core.MediaType;
+
+
+
+@Path("login")
+public class UsuariosResource {
+	
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Usuario getUser() {
+		Usuario usu= new Usuario("Una","Prueba",0);
+		return usu;
+	}
+	
+	@GET
+	@Path("all")
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<Usuario> getUsuarios(){
+		List<Usuario> usuarios = new ArrayList<>();
+		usuarios.add(new Usuario("Una","Loko",2));
+		usuarios.add(new Usuario("Dos","Prue",3));
+		
+		return usuarios;
+		
+	}
+	
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void anadirUsuario(String usu) {
+		System.out.println("Usuario" +usu+ "anadido");
+	}
+
+}
