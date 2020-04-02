aea16f01514173f113a8b0b08bd95af847bad96d
pablolengaran <pablogaviria@opendeusto.es>
2020-04-02 15:51:47 +0200

    Actualizacion ClienteApp y LoginResources.

--- concesionario/src/main/java/concesionario/cliente/ClienteApp.java
+++ concesionario/src/main/java/concesionario/cliente/ClienteApp.java
@@ -3,7 +3,6 @@ package concesionario.cliente;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.List;
 
 import javax.swing.JOptionPane;
 import javax.swing.SwingUtilities;
@@ -14,12 +13,12 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.client.Entity;
-import javax.ws.rs.core.GenericType;
 
 
 import concesionario.servidor.datos.Usuario;
 import concesionario.cliente.ventana.VentanaLogin;
 import concesionario.cliente.ventana.VentanaMenuCliente;
+import concesionario.cliente.ventana.VentanasRegistroClientes;
 
 //Cambiar el Pom.xml para que luego nos runnee esta aplicaicon
 public class ClienteApp {
@@ -29,6 +28,7 @@ public class ClienteApp {
 	
 	private VentanaLogin vlogin;
 	private VentanaMenuCliente vmc;
+	private VentanasRegistroClientes vrc;
 	
 	
 	public static final String URL_SERVER = "http://localhost:8080/concesionario";
@@ -41,6 +41,7 @@ public class ClienteApp {
 		
 		vlogin = new VentanaLogin();
 		vlogin.setVisible(true);
+		
 		
 		vlogin.getButtonAceptar().addActionListener(new ActionListener() {
 			
@@ -57,22 +58,51 @@ public class ClienteApp {
 
  	            if(response.getStatus() == Status.OK.getStatusCode()) {
  	            	String str = response.readEntity(String.class);
- 	            	JOptionPane.showMessageDialog(vlogin, "Datos correctos, el tipo es " +str);
- 	            	if(str.equals("3")) {
- 	            		vmc = new VentanaMenuCliente(nombre);
- 	            		vmc.setVisible(true);
- 	            		vlogin.dispose();
- 	            	}
- 	            	
- 	            }else {
+ 	            	int tipoUsuario = Integer.parseInt(str);
+ 	            	switch (tipoUsuario) {
+ 	            		case 0:
+ 	            			JOptionPane.showMessageDialog(vlogin, "Sesion iniciada como admin");
+ 	            			break;
+ 	            		case 1:
+ 	            			JOptionPane.showMessageDialog(vlogin, "Sesion iniciada como mecanico");
+ 	            			break;
+	 	       		case 2:
+	 	       			JOptionPane.showMessageDialog(vlogin, "Sesion iniciada como comercial");
+	 	       			break;
+	 	       		case 3:
+	 	       			vmc = new VentanaMenuCliente();
+	 	       			vmc.setVisible(true);
+	 	       			vlogin.dispose();
+	 	       			break;
+	 	       		case 4:
+	 	       			JOptionPane.showMessageDialog(vlogin, "Sesion iniciada como depatamento de compras");
+	 	       			break;
+	 	       		}
+ 	            }else if (response.getStatus() == Status.NOT_ACCEPTABLE.getStatusCode()){
  	            	JOptionPane.showMessageDialog(vlogin, "Datos incorrectos", "Error", JOptionPane.ERROR_MESSAGE);
+ 	            } else {
+ 	            	int respuesta = JOptionPane.showConfirmDialog(vlogin, "Usuario no registrado ¿Desea registrarse?");
+ 	            	switch (respuesta) {
+					case 0:
+						vrc = new VentanasRegistroClientes(nombre, password);
+						vrc.setVisible(true);
+						vlogin.dispose();
+						break;
+					case 1:
+						JOptionPane.showMessageDialog(vlogin, "El usuario no sera registrado.");
+						break;
+					case 2:
+						vlogin.vaciarCampos();
+						break;
+					}
  	            }
 
 
  			}
 		});
-		
 	}
+	
+	
 	
 
 	 public static void main(String[] args) {
