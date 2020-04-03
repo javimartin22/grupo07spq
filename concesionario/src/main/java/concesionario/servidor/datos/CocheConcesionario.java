9b52d9ef627f62d487ede2eb2080ca0f26443843
pablolengaran <pablogaviria@opendeusto.es>
2020-04-03 21:10:12 +0200

    Cambio Clase CocheConcesionario -> nuevos atributos.

--- concesionario/src/main/java/concesionario/servidor/datos/CocheConcesionario.java
+++ concesionario/src/main/java/concesionario/servidor/datos/CocheConcesionario.java
@@ -5,11 +5,19 @@ public class CocheConcesionario {
 	private String marca;
 	private String modelo;
 	private int precio;
+	private int unidades;
+	private int cv;
+	private int numPuertas;
+	private String color;
 
-	public CocheConcesionario(String marca, String modelo, int precio) {
+	public CocheConcesionario(String marca, String modelo, int precio, int cv, int numPuertas, String color, int unidades) {
 		this.marca = marca;
 		this.modelo = modelo;
 		this.precio = precio;
+		this.color = color;
+		this.cv = cv;
+		this.numPuertas = numPuertas;
+		this.unidades = unidades;
 	}
 	
 	public CocheConcesionario() {
@@ -37,5 +45,37 @@ public class CocheConcesionario {
 
 	public void setPrecio(int precio) {
 		this.precio = precio;
+	}
+
+	public int getUnidades() {
+		return unidades;
+	}
+
+	public void setUnidades(int unidades) {
+		this.unidades = unidades;
+	}
+
+	public int getCv() {
+		return cv;
+	}
+
+	public void setCv(int cv) {
+		this.cv = cv;
+	}
+
+	public int getNumPuertas() {
+		return numPuertas;
+	}
+
+	public void setNumPuertas(int numPuertas) {
+		this.numPuertas = numPuertas;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
 	}
 }
