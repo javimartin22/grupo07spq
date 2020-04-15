eac947a5a492cf86ba5fb552d1a1a3ffec2f70f8
pablo <pablo@192.168.0.23>
2020-04-15 18:55:52 +0200

    Creacion VentanaInformacionCocheConcesionario.

---
+++ concesionario/src/main/java/concesionario/cliente/ventana/VentanaInformacionCocheConcesionario.java
@@ -0,0 +1,133 @@
+package concesionario.cliente.ventana;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import concesionario.cliente.controller.LoginController;
+import concesionario.servidor.datos.CocheConcesionario;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class VentanaInformacionCocheConcesionario extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -543914344118437679L;
+	private JPanel contentPane;
+	private LoginController loginController;
+	
+	public VentanaInformacionCocheConcesionario(LoginController loginController, String nickname, CocheConcesionario coche) {
+		setResizable(false);
+		setTitle("Informacion del Vehiculo");
+		this.loginController = loginController;
+		ventanaInformacionCocheConcesionario(loginController, nickname, coche);
+	}
+	
+	/**
+	 * Create the frame.
+	 */
+	public void ventanaInformacionCocheConcesionario(LoginController loginController, String nickname, CocheConcesionario coche) {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 427, 386);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Marca:");
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblNewLabel.setBounds(22, 28, 174, 14);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Marca:");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblNewLabel_1.setBounds(22, 62, 174, 14);
+		contentPane.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("Precio:");
+		lblNewLabel_2.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblNewLabel_2.setBounds(22, 95, 174, 14);
+		contentPane.add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_2_1 = new JLabel("Unidades Disponibles: ");
+		lblNewLabel_2_1.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblNewLabel_2_1.setBounds(22, 133, 174, 23);
+		contentPane.add(lblNewLabel_2_1);
+		
+		JLabel lblNewLabel_2_2 = new JLabel("CV:");
+		lblNewLabel_2_2.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblNewLabel_2_2.setBounds(22, 179, 174, 14);
+		contentPane.add(lblNewLabel_2_2);
+		
+		JLabel lblNewLabel_2_2_1 = new JLabel("Numero de Puertas:");
+		lblNewLabel_2_2_1.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblNewLabel_2_2_1.setBounds(22, 212, 174, 14);
+		contentPane.add(lblNewLabel_2_2_1);
+		
+		JLabel lblNewLabel_2_2_1_1 = new JLabel("Color: ");
+		lblNewLabel_2_2_1_1.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblNewLabel_2_2_1_1.setBounds(22, 248, 174, 14);
+		contentPane.add(lblNewLabel_2_2_1_1);
+		
+		JLabel lblNewLabel_3 = new JLabel(coche.getMarca());
+		lblNewLabel_3.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		lblNewLabel_3.setBounds(206, 28, 122, 14);
+		contentPane.add(lblNewLabel_3);
+		
+		JLabel lblNewLabel_3_1 = new JLabel(coche.getModelo());
+		lblNewLabel_3_1.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		lblNewLabel_3_1.setBounds(206, 62, 122, 14);
+		contentPane.add(lblNewLabel_3_1);
+		
+		JLabel lblNewLabel_3_2 = new JLabel(coche.getPrecio() + "");
+		lblNewLabel_3_2.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		lblNewLabel_3_2.setBounds(206, 95, 122, 14);
+		contentPane.add(lblNewLabel_3_2);
+		
+		JLabel lblNewLabel_3_3 = new JLabel(coche.getUnidades() + "");
+		lblNewLabel_3_3.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		lblNewLabel_3_3.setBounds(206, 138, 122, 14);
+		contentPane.add(lblNewLabel_3_3);
+		
+		JLabel lblNewLabel_3_4 = new JLabel(coche.getCv() + "");
+		lblNewLabel_3_4.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		lblNewLabel_3_4.setBounds(206, 180, 122, 14);
+		contentPane.add(lblNewLabel_3_4);
+		
+		JLabel lblNewLabel_3_5 = new JLabel(coche.getNumPuertas() + "");
+		lblNewLabel_3_5.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		lblNewLabel_3_5.setBounds(206, 213, 122, 14);
+		contentPane.add(lblNewLabel_3_5);
+		
+		JLabel lblNewLabel_3_6 = new JLabel(coche.getColor());
+		lblNewLabel_3_6.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		lblNewLabel_3_6.setBounds(206, 249, 122, 14);
+		contentPane.add(lblNewLabel_3_6);
+		
+		JButton btnNewButton = new JButton("Regresar");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				VentanaVisualizarCatalogo vvc = new VentanaVisualizarCatalogo(loginController, nickname);
+				vvc.setVisible(true);
+				dispose();
+			}
+		});
+		btnNewButton.setBounds(73, 302, 89, 23);
+		contentPane.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Anyadir a Favoritos");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton_1.setBounds(226, 302, 147, 23);
+		contentPane.add(btnNewButton_1);
+	}
+}
