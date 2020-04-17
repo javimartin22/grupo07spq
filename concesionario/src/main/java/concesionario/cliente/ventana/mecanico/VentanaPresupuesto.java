71c425798ef2cb6d416dddf3e5c2320af7fba32c
pablo <pablo@192.168.0.23>
2020-04-17 12:49:50 +0200

    Añadido VentanaPresupuesto (contiene metodos para la creacin de la lista de piezas que se van a utilizar y creacón de la fecha).

---
+++ concesionario/src/main/java/concesionario/cliente/ventana/mecanico/VentanaPresupuesto.java
@@ -0,0 +1,189 @@
+package concesionario.cliente.ventana.mecanico;
+
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import concesionario.cliente.controller.Controller;
+import concesionario.datos.Presupuesto;
+
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.awt.event.ActionEvent;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextPane;
+import javax.swing.JTextField;
+import javax.swing.JSpinner;
+import java.awt.Font;
+
+public class VentanaPresupuesto extends JFrame {
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private Controller controller;
+	private JTextField textField;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTextField textField_4;
+	
+	public VentanaPresupuesto(Controller controller, String nickname) {
+		setResizable(false);
+		this.controller = controller;
+		ventanaPresupuesto(nickname);
+	}
+
+	
+	public void ventanaPresupuesto(String nickname) {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 452, 471);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnNewButton = new JButton("Regresar");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				VentanaMenuMecanico vmm = new VentanaMenuMecanico(controller, nickname);
+				vmm.setVisible(true);
+				dispose();
+			}
+		});
+		btnNewButton.setBounds(95, 390, 89, 23);
+		contentPane.add(btnNewButton);
+		
+		
+		
+		JLabel lblNewLabel = new JLabel("DNI Cliente:");
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 12));
+		lblNewLabel.setBounds(24, 32, 99, 14);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblMecanico = new JLabel("Mecanico:");
+		lblMecanico.setFont(new Font("Tahoma", Font.BOLD, 12));
+		lblMecanico.setBounds(24, 66, 119, 14);
+		contentPane.add(lblMecanico);
+		
+		JLabel lblMarcaDelVehiculo = new JLabel("Marca del Vehiculo:");
+		lblMarcaDelVehiculo.setFont(new Font("Tahoma", Font.BOLD, 12));
+		lblMarcaDelVehiculo.setBounds(24, 103, 119, 14);
+		contentPane.add(lblMarcaDelVehiculo);
+		
+		JLabel lblModeloDelVehiculo = new JLabel("Modelo del Vehiculo:");
+		lblModeloDelVehiculo.setFont(new Font("Tahoma", Font.BOLD, 12));
+		lblModeloDelVehiculo.setBounds(24, 144, 135, 14);
+		contentPane.add(lblModeloDelVehiculo);
+		
+		JLabel lblProblema = new JLabel("Problema:");
+		lblProblema.setFont(new Font("Tahoma", Font.BOLD, 12));
+		lblProblema.setBounds(24, 178, 119, 14);
+		contentPane.add(lblProblema);
+		
+		JLabel lblNumeroDePiezas = new JLabel("Numero de Piezas:");
+		lblNumeroDePiezas.setFont(new Font("Tahoma", Font.BOLD, 12));
+		lblNumeroDePiezas.setBounds(24, 215, 119, 14);
+		contentPane.add(lblNumeroDePiezas);
+		
+		JLabel lblObservaviones = new JLabel("Observaviones:");
+		lblObservaviones.setFont(new Font("Tahoma", Font.BOLD, 12));
+		lblObservaviones.setBounds(24, 285, 119, 14);
+		contentPane.add(lblObservaviones);
+		
+		JTextPane textPane = new JTextPane();
+		textPane.setToolTipText("");
+		textPane.setBounds(180, 285, 239, 79);
+		contentPane.add(textPane);
+		
+		textField = new JTextField();
+		textField.setBounds(180, 29, 239, 20);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		textField_2 = new JTextField();
+		textField_2.setColumns(10);
+		textField_2.setBounds(180, 100, 239, 20);
+		contentPane.add(textField_2);
+		
+		textField_3 = new JTextField();
+		textField_3.setColumns(10);
+		textField_3.setBounds(180, 141, 239, 20);
+		contentPane.add(textField_3);
+		
+		textField_4 = new JTextField();
+		textField_4.setColumns(10);
+		textField_4.setBounds(180, 175, 239, 20);
+		contentPane.add(textField_4);
+		
+		JSpinner spinner = new JSpinner();
+		spinner.setBounds(180, 212, 71, 20);
+		contentPane.add(spinner);
+		
+		JLabel lblNewLabel_1 = new JLabel("");
+		lblNewLabel_1.setBounds(180, 66, 239, 14);
+		contentPane.add(lblNewLabel_1);
+		
+		JLabel lblPrecio = new JLabel("Precio:");
+		lblPrecio.setFont(new Font("Tahoma", Font.BOLD, 12));
+		lblPrecio.setBounds(24, 253, 119, 14);
+		contentPane.add(lblPrecio);
+		
+		JSpinner spinner_1 = new JSpinner();
+		spinner_1.setBounds(180, 251, 71, 20);
+		contentPane.add(spinner_1);
+		
+		JButton btnNewButton_1 = new JButton("Crear Presupuesto");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				String dniCliente = textField.getText();
+				String mecanico = nickname;
+				String marca = textField_2.getText();
+				String modelo = textField_3.getText();
+				String problema = textField_4.getText();
+				int numPiezas = Integer.parseInt(spinner.getValue().toString());
+				String listaPiezas = crearPiezasString(numPiezas);
+				String observaciones = textPane.getText();
+				int precio = Integer.parseInt(spinner_1.getValue().toString());
+				String fecha = parseFecha();
+				Presupuesto presupuesto = new Presupuesto(dniCliente, mecanico, marca, modelo, problema, numPiezas, listaPiezas, observaciones, precio, fecha);
+				registroPresupuesto(presupuesto);
+			}
+		});
+		btnNewButton_1.setBounds(231, 390, 144, 23);
+		contentPane.add(btnNewButton_1);
+		
+		
+	}
+	
+	public void registroPresupuesto(Presupuesto presupuesto) {
+		JOptionPane.showMessageDialog(contentPane, "Registrando...");
+	}
+	
+	public String crearPiezasString(int numPiezas) {
+		String listaPiezas = "";
+		ArrayList<String> piezas = new ArrayList<String>();
+		for (int i = 0; i < numPiezas; i++) {
+			int j = i + 1;
+			String pieza = JOptionPane.showInputDialog("Introduzca la pieza " + j + ":");
+			piezas.add(pieza);
+		}
+		
+		for (int i = 0; i < piezas.size(); i++) {
+			if (i == 0) {
+				listaPiezas = piezas.get(i);
+			} else {
+				listaPiezas = listaPiezas + ", " + piezas.get(i);
+			}
+		}
+		return listaPiezas;
+	}
+	
+	public String parseFecha() {
+		String fecha = "";
+		Calendar c = Calendar.getInstance();
+		fecha = c.get(Calendar.DAY_OF_MONTH) + "/" + c.get(Calendar.MONTH) + "/" + c.get(Calendar.YEAR) + " - " + c.get(Calendar.HOUR_OF_DAY) + ":" + c.get(Calendar.MINUTE); 
+		return fecha;
+	}
+}
