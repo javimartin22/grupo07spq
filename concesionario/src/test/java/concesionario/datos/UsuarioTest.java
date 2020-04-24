93f25cda0897538dad0a63410bd02b8d1e923330
Pablo Gaviria <pablogaviria@opendeusto.es>
2020-04-24 13:55:02 +0200

    Añadido UsuarioTest.

--- concesionario/src/test/java/concesionario/datos/UsuarioTest.java
+++ concesionario/src/test/java/concesionario/datos/UsuarioTest.java
@@ -1,5 +1,50 @@
 package concesionario.datos;
 
-public class UsuarioTest {
+import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import junit.framework.JUnit4TestAdapter;
+
+public class UsuarioTest {
+	private Usuario usuario;
+
+	public static junit.framework.Test suite() {
+		return new JUnit4TestAdapter(ComercialTest.class);
+	}
+	@Before
+	public void setUp() {
+		usuario = new Usuario("Jorgico", "1234", 1);
+	}
+	
+	//Test Getters:
+	@Test
+	public void testGetNickname() {
+		assertEquals("Jorgico", usuario.getNickname());
+	}
+	@Test
+	public void testGetContrasenya() {
+		assertEquals("1234", usuario.getContrasenya());
+	}
+	@Test
+	public void testGetTipo() {
+		assertEquals(1, usuario.getTipo());
+	}
+	
+	//Test Setters:
+	@Test
+	public void testSetNickname() {
+		usuario.setNickname("Jorgico1");
+		assertEquals("Jorgico1", usuario.getNickname());
+	}
+	@Test
+	public void testSetContrasenya() {
+		usuario.setContrasenya("12345");
+		assertEquals("12345", usuario.getContrasenya());
+	}
+	@Test
+	public void testSetTipo() {
+		assertEquals(2, usuario.getTipo());
+	}
 }
