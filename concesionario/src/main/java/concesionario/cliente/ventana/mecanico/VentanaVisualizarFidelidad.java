87ce5599606f01e4ede0d812f8b17b04b827ac2c
Mikel <mikelromero11@gmail.com>
2020-04-24 11:35:03 +0200

    Ventana para que los mecanicos visualicen la fidelidad de los clientes.

---
+++ concesionario/src/main/java/concesionario/cliente/ventana/mecanico/VentanaVisualizarFidelidad.java
@@ -0,0 +1,99 @@
+package concesionario.cliente.ventana.mecanico;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.List;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
+
+import concesionario.cliente.controller.MecanicoController;
+import concesionario.datos.CocheTaller;
+
+import java.awt.BorderLayout;
+
+
+
+
+
+public class VentanaVisualizarFidelidad extends JFrame {
+
+	private static final long serilVersionUID = 1L;
+	private JPanel contentPane;
+	private MecanicoController mecanicoController;
+	private JTable table1;
+	
+	public VentanaVisualizarFidelidad(MecanicoController mecanicoController, String nickname) {
+		this.mecanicoController = mecanicoController;
+		iniciarVentanaVisualizarFidelidad(nickname);
+	}
+	
+	
+	/**
+	 * Create the frame
+	 * @return 
+	 */
+	
+	public void iniciarVentanaVisualizarFidelidad(String nickname) {
+		setResizable(false);
+		setTitle("Fidelidad de los clientes:");
+		setBounds(100, 100, 992, 400);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);		
+		getContentPane().setLayout(null);
+		setLocationRelativeTo(null);
+		
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(6, 18, 980, 274);
+		getContentPane().add(scrollPane);
+		
+		table1 = new JTable();
+		scrollPane.setViewportView(table1);
+		
+		JButton btnNewButton = new JButton("Cargar");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//cargarTabla(table1);
+			}
+		});
+		btnNewButton.setBounds(565, 303, 117, 29);
+		getContentPane().add(btnNewButton);
+		
+		JButton btnVolve = new JButton("Volver");
+		btnVolve.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				/**
+				 * metodo para volcer a pa ppagina anterior
+				 */
+			}
+		});
+		btnVolve.setBounds(294, 304, 117, 29);
+		getContentPane().add(btnVolve);
+	}
+	
+	/*private void cargarTabla(JTable table) {
+		List<ClienteFidelidad> cliente = loginController.cargarClientesFieles();
+		String[] columnNames = {"DNI", "Veces en el taller"};
+		
+		if (!cliente.isEmpty()) {
+			 DefaultTableModel model = new DefaultTableModel();
+			   table.setModel(model);
+			   model.setColumnIdentifiers(columnNames);
+			   
+			   for (ClienteFidelidad c : cliente) {
+				   Object[] o = new Object[2];
+				   o[0] = c.getDni();
+				   o[1] = c.getVeces();
+				   model.addRow(o);
+				 }
+		} else {
+			System.out.println("llega mal");
+		}
+		
+	}*/
+	
+}
