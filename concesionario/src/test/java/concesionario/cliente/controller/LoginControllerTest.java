82454f2be24e5439c7355fafa100f0e3ddd2f177
javi <javi.martin22@opendeusto.es>
2020-04-24 23:04:45 +0200

    LoginControllerTest 100

--- concesionario/src/test/java/concesionario/cliente/controller/LoginControllerTest.java
+++ concesionario/src/test/java/concesionario/cliente/controller/LoginControllerTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -18,7 +19,7 @@ import concesionario.datos.Usuario;
 
 
 
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(MockitoJUnitRunner.Silent.class) 
 public class LoginControllerTest {
 
 	public LoginController loginController;
@@ -35,15 +36,38 @@ public class LoginControllerTest {
 	@Test
 	public void testLogin() {
 		String tipo_admin = "0";
+		String tipo_not_accept = "5";
+		String tipo_not_found = "6";
 		
 		Response response = Mockito.mock(Response.class);
-		Mockito.when(response.getStatus()).thenReturn(200);
+		Mockito.when(response.getStatus()).thenReturn(Status.OK.getStatusCode());
 		Mockito.when(response.readEntity(Mockito.any(Class.class))).thenReturn(tipo_admin);
 		
 		when(cliente.login(any(Usuario.class))).thenReturn(response);
 		
 		assertTrue(loginController.login("admin", "admin") == 0);
+		
+		
+		Response response1 = Mockito.mock(Response.class);
+		Mockito.when(response1.getStatus()).thenReturn(Status.NOT_ACCEPTABLE.getStatusCode());
+		
+		when(cliente.login(any(Usuario.class))).thenReturn(response1);
+		
+		assertTrue(loginController.login("admin", "ad") == 5);
+		
+		Response response2 = Mockito.mock(Response.class);
+		Mockito.when(response2.getStatus()).thenReturn(Status.NOT_FOUND.getStatusCode());
+		Mockito.when(response2.readEntity(Mockito.any(Class.class))).thenReturn(tipo_not_found);
+		
+		when(cliente.login(any(Usuario.class))).thenReturn(response2);
+		
+		assertTrue(loginController.login("ad", "ad") == 6);
+		
 	}
+	
+	
+	
+	
 	
 
 	
