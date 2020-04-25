def80abbd93437bc192e40cfc8608f0d4199d176
Mikel <mikelromero11@gmail.com>
2020-04-25 12:15:40 +0200

    Como se ha cambiado el ClienteFidelidad, he ehcho unos ajustes en su clalse test

--- concesionario/src/test/java/concesionario/datos/ClienteFidelidadTest.java
+++ concesionario/src/test/java/concesionario/datos/ClienteFidelidadTest.java
@@ -17,45 +17,13 @@ public class ClienteFidelidadTest {
 	@Before
 	public void setUp() {
 		cliente = new ClienteFidelidad();
-		cliente = new ClienteFidelidad("12345678K", "user", 1, "pass", "javi", "martin", "masculino", "javi@gmail.com", "bilbo", 46009, "kalea", "666777872", 0);
+		cliente = new ClienteFidelidad("12345678K", 2);
 	}
 	
 	//Test Getters
 		@Test
-		public void testGetNombre() {
-			assertEquals("javi", cliente.getNombre());
-		}
-		@Test
-		public void testGetDNI() {
-			assertEquals("12345678K", cliente.getDNI());
-		}
-		@Test
-		public void testGetApellido() {
-			assertEquals("martin", cliente.getApellido());
-		}
-		@Test
-		public void testGetEmail() {
-			assertEquals("javi@gmail.com", cliente.getEmail());
-		}
-		@Test
-		public void testGetSexo() {
-			assertEquals("masculino", cliente.getSexo());
-		}
-		@Test
-		public void testGetCiudad() {
-			assertEquals("bilbo", cliente.getCiudad());
-		}
-		@Test
-		public void testGetCodigoPostal() {
-			assertEquals(46009, cliente.getCodigoPostal());
-		}
-		@Test
-		public void testGetDireccion() {
-			assertEquals("kalea", cliente.getDireccion());
-		}
-		@Test
-		public void testGetNumeroTelefono() {
-			assertEquals("666777872", cliente.getNumeroTelefono());
+		public void testGetDni() {
+			assertEquals("12345678K", cliente.getDni());
 		}
 		@Test
 		public void testGetFidelidad() {
@@ -64,49 +32,9 @@ public class ClienteFidelidadTest {
 		
 		//Setters
 		@Test 
-		public void testSetDNI() {
-			cliente.setDNI("11122233V");
-			assertEquals("11122233V", cliente.getDNI());
-		}
-		@Test 
-		public void testSetNombre() {
-			cliente.setNombre("Leire");
-			assertEquals("Leire", cliente.getNombre());
-		}
-		@Test 
-		public void testSetApellido() {
-			cliente.setApellido("Lopez");
-			assertEquals("Lopez", cliente.getApellido());
-		}
-		@Test 
-		public void testSetEmail() {
-			cliente.setEmail("hola@gmail.com");
-			assertEquals("hola@gmail.com", cliente.getEmail());
-		}
-		@Test 
-		public void testSetSexo() {
-			cliente.setSexo("Femenino");
-			assertEquals("Femenino", cliente.getSexo());
-		}
-		@Test 
-		public void testSetCiudad() {
-			cliente.setCiudad("Paris");
-			assertEquals("Paris", cliente.getCiudad());
-		}
-		@Test 
-		public void testSetCodigoPostal() {
-			cliente.setCodigoPostal(48012);
-			assertEquals(48012, cliente.getCodigoPostal());
-		}
-		@Test 
-		public void testSetDireccion() {
-			cliente.setDireccion("calle");
-			assertEquals("calle", cliente.getDireccion());
-		}
-		@Test 
-		public void testSetNumeroTelefono() {
-			cliente.setNumeroTelefono("654654654");
-			assertEquals("654654654", cliente.getNumeroTelefono());
+		public void testSetDni() {
+			cliente.setDni("11122233V");
+			assertEquals("11122233V", cliente.getDni());
 		}
 		@Test
 		public void testSetFidelidad() {
