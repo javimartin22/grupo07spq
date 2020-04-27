9feb1fa5888d768840b5c8c4f6799477ddd8a9b3
yeray <yeraybellanco@opendeusto.es>
2020-04-28 01:07:06 +0200

    Actualizacion clase VentanaInformacionProveedor

--- concesionario/src/main/java/concesionario/cliente/ventana/departamentoCompras/VentanaInformacionProveedores.java
+++ concesionario/src/main/java/concesionario/cliente/ventana/departamentoCompras/VentanaInformacionProveedores.java
@@ -2,36 +2,30 @@ package concesionario.cliente.ventana.departamentoCompras;
 
 
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
-
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import concesionario.cliente.controller.DepartmentoComprasController;
+import concesionario.datos.Proveedor;
+import java.awt.Font;
 
 public class VentanaInformacionProveedores extends JFrame {
 
+	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private DepartmentoComprasController departamentoComprasController;
 	
 	
 	
-	public VentanaInformacionProveedores(DepartmentoComprasController departmentoComprasController, int tipo) {
+	public VentanaInformacionProveedores(DepartmentoComprasController departmentoComprasController,Proveedor proveedor, String nickname) {
 		setResizable(false);
 		this.departamentoComprasController = departmentoComprasController;
-		iniciarVentanaInformacionProveedores(tipo);
+		iniciarVentanaInformacionProveedores(proveedor,nickname);
 	}
 	
 	
@@ -39,7 +33,7 @@ public class VentanaInformacionProveedores extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public void  iniciarVentanaInformacionProveedores(int tipo) {
+	public void  iniciarVentanaInformacionProveedores(Proveedor proveedor, String nickname) {
 		setTitle("Informacion proveedores");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 424, 211);
@@ -48,41 +42,44 @@ public class VentanaInformacionProveedores extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-//		con = BD.initBD("Proveedores");
-//		st = BD.usarCrearTablasBD(con);
 		
-		JLabel lblNombre = new JLabel("Nombre");
+		JLabel lblNombre = new JLabel("Nombre:");
+		lblNombre.setFont(new Font("Lucida Grande", Font.BOLD, 13));
 		lblNombre.setBounds(6, 32, 61, 16);
 		contentPane.add(lblNombre);
 		
-		JLabel lblnomProv = new JLabel();
-		lblnomProv.setBounds(169, 32, 61, 16);
+		JLabel lblnomProv = new JLabel(proveedor.getNombre());
+		lblnomProv.setBounds(169, 32, 85, 16);
 		contentPane.add(lblnomProv);
 		
-		JLabel lblPais = new JLabel("Pais");
+		JLabel lblPais = new JLabel("Pais:");
+		lblPais.setFont(new Font("Lucida Grande", Font.BOLD, 13));
 		lblPais.setBounds(6, 71, 61, 16);
 		contentPane.add(lblPais);
 		
-		JLabel lblpaisProv = new JLabel();
+		JLabel lblpaisProv = new JLabel(proveedor.getPais());
 		lblpaisProv.setBounds(169, 71, 156, 16);
 		contentPane.add(lblpaisProv);
 		
-		JLabel lblTiposDePieza = new JLabel("Tipos de piezas");
-		lblTiposDePieza.setBounds(6, 115, 102, 16);
+		JLabel lblTiposDePieza = new JLabel("Tipos de piezas:");
+		lblTiposDePieza.setFont(new Font("Lucida Grande", Font.BOLD, 13));
+		lblTiposDePieza.setBounds(6, 115, 135, 16);
 		contentPane.add(lblTiposDePieza);
 		
-		JLabel lbltiposProv = new JLabel();
+		JLabel lbltiposProv = new JLabel(proveedor.getTipoPiezas());
 		lbltiposProv.setBounds(169, 115, 276, 16);
 		contentPane.add(lbltiposProv);
 		
 		JButton btnSalir = new JButton("Salir");
 		btnSalir.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
+			public void actionPerformed(ActionEvent e) { 
+				VentanaComprarPiezas vcp = new VentanaComprarPiezas(departamentoComprasController, nickname);
+				vcp.setVisible(true);
 				dispose();
 			}
 		});
 		
-		btnSalir.setBounds(169, 154, 117, 29);
+		btnSalir.setBounds(147, 154, 117, 29);
 		contentPane.add(btnSalir);
 
 		
